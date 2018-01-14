0d7fbadd1842bc5eb3a848ec2622087c4b6440c2
bcameron1489 <bcameron1489@gmail.com>
2018-01-14 06:26:29 -0500

    Add changes to users to test template issues

--- app/routes/trails.js
+++ app/routes/trails.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model (data) {
+  model () {
+    console.log('we here now')
     return this.get('store').findAll('trail');
-  }
+  },
 });
