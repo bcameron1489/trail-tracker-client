b262958c3a960e268ed498ebe113476b6e90c3d0
bcameron1489 <bcameron1489@gmail.com>
2018-01-14 18:55:32 -0500

    Remove all files from ember template
    Remove all files from ember template and prepare to install default
    browser template.  Save ember branch for later use.

--- app/models/trail.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  mountain: DS.attr('string'),
-  difficulty: DS.attr('string'),
-  rating: DS.attr('integer')
-});
