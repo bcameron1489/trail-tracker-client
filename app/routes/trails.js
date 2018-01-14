b9f010f568621280b3d2781d15b7e1ed044a73e0
bcameron1489 <bcameron1489@gmail.com>
2018-01-14 04:15:50 -0500

    Build out templates for trails

--- app/routes/trails.js
+++ app/routes/trails.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model () {
-      return this.get('store').findAll('list');
-    }
+  model (data) {
+    return this.get('store').findAll('trail');
+  }
 });
