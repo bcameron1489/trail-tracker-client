0fb3cec8c74c002c1639800e3cc5af7f7f9fe6b4
bcameron1489 <bcameron1489@gmail.com>
2018-01-14 19:46:37 -0500

    Set proper paths

--- assets/scripts/api/auth-api.js
+++ assets/scripts/api/auth-api.js
@@ -1,7 +1,7 @@
 'use strict'
 // const store = require('./store')
 
-const config = require('./config')
+const config = require('../config')
 
 const signUp = function (data) {
   return $.ajax({
--- assets/scripts/events/auth-events.js
+++ assets/scripts/events/auth-events.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const getFormFields = require('../../lib/get-form-fields')
-const api = require('./api')
-const ui = require('./ui')
+const getFormFields = require('../../../lib/get-form-fields')
+const api = require('../api/auth-api.js')
+const ui = require('../ui/auth-ui.js')
 // const store = require('./store')
 
 const onSignUp = function (event) {
