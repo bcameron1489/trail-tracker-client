5d0f62cfc5ca6b348b873df49159182e68c232f6
bcameron1489 <bcameron1489@gmail.com>
2018-01-15 21:37:59 -0500

    Change the production url
    Change production url in config.js to read back end url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://trail-tracker-api.herokuapp.com/'
+    // https://ga-wdi-boston.herokuapp.com
   }
 }
 
